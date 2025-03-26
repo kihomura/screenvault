efcddfd9774b4e6accc677bb7c7ccc2dd33e4f7e
kihomura <kihomura552@gmail.com>
2025-03-26 15:28:42 +1300

    Update cors config

---
+++ server/src/main/java/com/kihomura/screenvault/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.kihomura.screenvault.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowCredentials(true)
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
