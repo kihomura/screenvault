9577423b2547819051aec6369d85d3ea75d40a43
kihomura <kihomura552@gmail.com>
2025-05-07 16:41:53 +1200

    Prepare for first deploy.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   css: {
     preprocessorOptions: {
       scss: {
@@ -11,6 +19,7 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
+    port: process.env.PORT || 5173,
     proxy: {
       '/auth': {
         target: 'http://localhost:5555',
@@ -26,5 +35,9 @@ export default defineConfig({
         secure: false
       }
     }
+  },
+  preview: {
+    port: process.env.PORT || 5173,
+    host: '0.0.0.0',
   }
 })
